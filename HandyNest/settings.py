632c13663349bb089274af86fa9b6c1d69f02e77
Arnica15260 <arnicasarker4@gmail.com>
2025-05-10 01:02:54 +0600

    certificate

--- HandyNest/settings.py
+++ HandyNest/settings.py
@@ -133,5 +133,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/user/login/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- HandyNest/urls.py
+++ HandyNest/urls.py
@@ -33,8 +33,6 @@ urlpatterns = [
     path('user/',include('users.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
